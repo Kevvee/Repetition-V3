fc8e4e6b609c2bf79a140d9462e45962fd689605
Kevvee <Kevjoh.044@gmail.com>
2022-01-17 13:28:36 +0100

    ⚡️ Repitition (fett coolt)

---
+++ Repitition-3.py
@@ -0,0 +1,16 @@
+pris = input("Pris? ")
+
+if pris == "exit":
+    exit()
+
+pris = float(pris)
+
+if pris > 35:
+    print("Du passerar gå och inkaserar 30 öre")
+
+
+elif pris < 35:
+    print("Du får 3")
+    
+
+
